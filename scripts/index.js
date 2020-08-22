911e54d73ee9eb8416154722c29581f8ba8d34b9
Edward Reyes <reyes.edwardf@gmail.com>
2020-08-21 23:02:22 -0700

    changed html

--- scripts/index.js
+++ scripts/index.js
@@ -99,7 +99,7 @@ projects = [
         long_description:
             `<h5>Concept</h5>
             <p>Takes in information about employees and generates an HTML webpage that displays summaries for each person. Since testing is a key piece in making code maintainable, you will also be ensuring that all unit tests pass.</p>
-            <h5>Site information Display/h5>
+            <h5>Site information Display</h5>
             <p>Displays Employee name, id, email, and/or office number, GitHub user, school</p>
             `,
         images: [
