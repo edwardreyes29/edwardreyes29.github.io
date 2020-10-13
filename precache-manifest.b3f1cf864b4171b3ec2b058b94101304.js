7446411fb255473e348ed6234d36ebb75aabdafe
Edward Reyes <reyes.edwardf@gmail.com>
2020-10-12 21:12:02 -0700

    Updates

---
+++ precache-manifest.b3f1cf864b4171b3ec2b058b94101304.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b12df7b0e1cd9d201793c6f16d40ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c573871891a4af5370d",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "11aa65934bea49e86e66",
+    "url": "/static/css/main.5cd9515c.chunk.css"
+  },
+  {
+    "revision": "4c573871891a4af5370d",
+    "url": "/static/js/2.40dfa7fb.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.40dfa7fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11aa65934bea49e86e66",
+    "url": "/static/js/main.3fed2514.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "60393fc190af27c265738ebbeb6cd185",
+    "url": "/static/media/csun-logo.60393fc1.svg"
+  },
+  {
+    "revision": "7cc20f5efed3b78b72ac3f9d9a740258",
+    "url": "/static/media/jumbotron-wallpaper.7cc20f5e.jpg"
+  },
+  {
+    "revision": "6195610deea91882564c8124bdc7636b",
+    "url": "/static/media/logo-white.6195610d.svg"
+  },
+  {
+    "revision": "c62d401ca6b0dd5c33a2c9d8d4771b60",
+    "url": "/static/media/logo.c62d401c.svg"
+  }
+]);
